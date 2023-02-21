c4c0cd12e93d05c556a5f05759bad5424ae04f4a
avinash275 <104912103+avinash275@users.noreply.github.com>
2023-02-21 12:00:38 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,32 @@
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<style>
+    img {
+        width: 100%;
+    }
+    .container{
+        display:flex;
+        justify-content: center;
+        justify-items: center;
+        /* align-items: center; */
+    }
+    </style>
+<?php include './include/header.php' ?>
+<div class="section-title ">
+    <h1 class="text-center my-5">About Us</h1>
+</div>
+<div class="row content">
+    <div class="container">
+        <div class="col-lg-6 ">
+            <img class="rounded img-fluid float-left" src="https://images.unsplash.com/photo-1557426272-fc759fdf7a8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" />
+        </div>
+        <div class="col-lg-6 pt-4 pt-lg-0 ">
+            <p>
+                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+                Lorem, ipsum dolor Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio iste sunt voluptas mollitia ea ab sapiente enim, reiciendis aperiam nisi hic perspiciatis omnis, ipsa iure minus illum consectetur totam alias! Dolores ab quidem eligendi omnis odit illo consectetur quas maiores? Eveniet repudiandae voluptatibus repellat quia non ab beatae rem maiores earum. Maxime, deleniti fuga cum fugit facilis vero, itaque sed ratione dolorem, iusto impedit reprehenderit ullam delectus modi inventore ad? In at recusandae, a minima numquam incidunt suscipit ad voluptate iusto! Libero beatae ipsam ea sequi veniam, harum impedit aperiam. Illum temporibus, tempora provident unde aspernatur nobis voluptatem. Quis porro non iste quasi explicabo vitae voluptate, ex, velit ratione cumque, quibusdam ipsum earum eaque iusto blanditiis quos sapiente ea maxime quae tempore sequi? Cumque repellat obcaecati unde praesentium officia labore, ipsam voluptates et tempora consectetur quisquam delectus eveniet minus ipsa aliquam dolor odit quas hic eaque necessitatibus voluptatem possimus eum recusandae soluta! Incidunt iure doloremque nesciunt hic mollitia debitis eaque aspernatur porro, maxime quis expedita! Sequi, corporis eveniet tempore eos culpa quasi veritatis quia placeat? Ullam autem sunt voluptatem delectus perspiciatis a eos recusandae minima, suscipit quia est doloribus molestias distinctio fugiat ad in vero rerum eaque? Fugit ab nemo minima neque, repellat velit laboriosam officia delectus deleniti quos, ad eaque id blanditiis voluptatum labore ex amet? Expedita alias recusandae ad ex fugiat vitae quis eos eum iusto corporis. Sint natus, eligendi expedita tempore veniam perspiciatis commodi repellendus, voluptatem deleniti adipisci qui debitis fugiat porro vitae reiciendis dolor blanditiis quos eius maiores explicabo quod minus neque illo. Porro eaque accusantium tempora sapiente error ab nobis, quasi explicabo, delectus veritatis quas vero saepe cum impedit itaque minus odit similique autem iusto! Enim, nemo quia eaque in, molestias fuga asperiores, neque excepturi voluptatum vitae optio officia dolor exercitationem suscipit sed cupiditate autem iusto molestiae natus sequi harum voluptates blanditiis. Voluptates reiciendis, praesentium incidunt laudantium voluptatum odit ducimus. Pariatur aperiam eos atque saepe eaque porro blanditiis sequi, suscipit assumenda eveniet sed esse inventore, laborum debitis itaque, rerum molestiae ipsam ad neque molestias impedit.
+            </p>
+            <a href="ourservices.php" class="btn-learn-more">Learn More</a>
+        </div>
+    </div>
+</div>
---
+++ ourservices.php
@@ -0,0 +1,33 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<style>
+    img {
+        width: 100%;
+    }
+    /* .container{
+        display:flex;
+        justify-content: center;
+        justify-items: center;
+    } */
+    </style>
+<?php include './include/header.php' ?>
+<div class="container my-5">
+            <h1 class="text-dark text-center my-5">Our Services</h1>
+            <div class="container">
+            <div class="row row-cols-2">       
+              <div class="col ">
+              <h3>Manage Everything at one place</h3>   
+                <div class="container col my-5">
+                   <p class="lead"> To see all Customers</p>
+                   <a href="customer.php"><button class="btn btn-primary" type="button" >Get Started</button></a>
+                </div>
+                <div class="container col my-5">
+                   <p class="lead"> To see all Transaction</p>
+                   <a href="transaction.php"><button class="btn btn-primary" type="button" >Get Started</button></a>
+                </div>
+               
+              </div>
+
+              <div class="col">
+              <img src="https://images.unsplash.com/photo-1616514197671-15d99ce7a6f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80" class="rounded img-fluid float-right" alt="...">
+              </div>
+            </div>
